a527dbc50429fa3552b94c12f76158ee2ce4ad36
David Lawson <dave.lawson.barbar@gmail.com>
2024-11-19 13:45:35 -0500

    got debugger running inside nvim

--- init.lua
+++ init.lua
@@ -5,3 +5,6 @@ vim.api.nvim_create_user_command("TODO", "e ~/todo", {})
 vim.api.nvim_create_user_command("RC", "e ~/.zshrc", {})
 vim.api.nvim_create_user_command("SC", "e ~/.config/nvim/nvim.shortcuts", {})
 vim.api.nvim_create_user_command("NOTES", "e ~/notes", {})
+vim.api.nvim_create_user_command("VIMRC", "e ~/.config/nvim/init.lua", {})
+
+vim.api.nvim_create_user_command("GO", "!go run %", {})
