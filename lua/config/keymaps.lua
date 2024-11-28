668ae3671e3fe34ca3c8f53d311f2694f64e002c
David Lawson <dave.lawson.barbar@gmail.com>
2024-11-28 11:50:53 -0500

    added some hotkeys for navigating the callstack in debug mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+local dap = require("dap")
+map("n", "<leader>bk", dap.up)
+map("n", "<leader>bj", dap.down)
