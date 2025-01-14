adec512fcbf4eb6ca56153ea0d88ce4901403418
David.Lawson <david.lawson@ticketmaster.com>
2025-01-14 11:04:13 -0500

    Add macos-specific bindings for moving lines and blocks of text around

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,10 @@ local map = LazyVim.safe_keymap_set
 local dap = require("dap")
 map("n", "<leader>bk", dap.up)
 map("n", "<leader>bj", dap.down)
+-- Some MacOS specific binding, since it handles alt-keys strangely
+map("n", "∆", ":m+1<cr>")
+map("n", "˚", ":m-2<cr>")
+map("v", "˚", ":m'<-2<cr>gv=gv")
+map("v", "∆", ":m'>+1<cr>gv=gv")
+map("i", "˚", "<Esc>:m-2<cr>")
+map("i", "∆", "<Esc>:m+1<cr>")
