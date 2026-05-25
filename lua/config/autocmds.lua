3ce9ad4e86e2ac95bcb9d7ccde0e9626f704f0cd
David Lawson <dave.lawson.barbar@gmail.com>
2026-05-25 12:27:44 -0400

    Add command :ReloadFromDisk which forces neovim to refresh all buffers and reparse the project so as to eliminate cached code errors.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,24 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_user_command("ReloadFromDisk", function()
+  vim.cmd("checktime")
+
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(bufnr) then
+      local name = vim.api.nvim_buf_get_name(bufnr)
+      local buftype = vim.bo[bufnr].buftype
+      local modified = vim.bo[bufnr].modified
+
+      if name ~= "" and buftype == "" and not modified then
+        vim.api.nvim_buf_call(bufnr, function()
+          vim.cmd("silent! edit!")
+        end)
+
+        vim.diagnostic.reset(nil, bufnr)
+      end
+    end
+  end
+
+  vim.cmd("silent! LspRestart")
+end, {})
---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,14 @@
+return {
+  "olimorris/codecompanion.nvim",
+  version = "^19.0.0",
+  opts = {
+    -- intential extra opts object
+    opts = {
+      log_level = "DEBUG",
+    },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+}
